13d481cbffbb36ad4542372ba1c08e42c013d9be
nosdeedson <josemariazumira@gmail.com>
2021-03-28 14:08:48 -0300

    cabecalho

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// function typeWriter(elemento){
+//     const nameArray = elemento.innerHTML.split('')
+//     elemento.innerHTML = ''
+//     nameArray.forEach((letra , i) => {
+//         setTimeout( () => elemento.innerHTML += letra, 75 * (i + 1) );
+//     });
+// }
+
+// const nome = document.querySelector('#ejs')
+// const curse = document.querySelector('#sisInfo')
+
+// typeWriter(nome);
+// typeWriter(curse)
